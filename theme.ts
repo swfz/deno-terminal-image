357ef1a31dbffa65ddc6e6e4c4860cb9f764d885
swfz <sawafuji.09@gmail.com>
2023-07-29 03:18:27 +0900

    feat: themeファイル作成、bundleするファイルを追加

---
+++ theme.ts
@@ -0,0 +1,14 @@
+
+const defaultTheme = {
+  topColors: ["#6797e8", "#a4e083", "#efb24a", "#ec7563"],
+  bottomColors: ["#6797e8", "#a4e083", "#efb24a", "#ec7563"],
+  topColor: "#555555",
+  bottomColor: "#555555",
+  cursorColor: "#ec80f7",
+  promptColor: "#efb24a",
+  bgColor: "#313d4f",
+  titleColor: "#FFFFFF",
+  commandColor: "#888888",
+}
+
+export {defaultTheme}

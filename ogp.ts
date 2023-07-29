cc9f99e990520c3a5d2ab3f1dbe7b54c126d887c
swfz <sawafuji.09@gmail.com>
2023-07-30 03:12:49 +0900

    refactor: Various render processes are combined into one function

--- ogp.ts
+++ ogp.ts
@@ -1,32 +1,21 @@
 import { createCanvas } from "https://deno.land/x/canvas/mod.ts";
-import {
-  breakLines,
-  renderBackground,
-  renderBottomStatusLine,
-  renderCommand,
-  renderCursor,
-  renderPrompt,
-  renderTitle,
-  renderTopStatusLine,
-} from "./render.ts";
+import { render } from "./render.ts";
 import { defaultTheme } from "./theme.ts";
-
 import { serveDir, serveFile } from "https://deno.land/std@0.141.0/http/file_server.ts";
 
 const port = 8080;
-const textLineBase = 100;
 
 const isValidColorCode = (str: string) => {
   return /^([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/.test(str);
 };
 
-const colorParamGetter = (url: URL, key: string) => {
+const getColor = (url: URL, key: string) => {
   const param = url.searchParams.get(key) ?? "";
 
   return isValidColorCode(param) ? `#${param}` : null;
 };
 
-const colorsParamGetter = (url: URL, key: string) => {
+const getColors = (url: URL, key: string) => {
   const param = url.searchParams.get(key) ?? null;
 
   if (!param) {
@@ -34,6 +23,56 @@ const colorsParamGetter = (url: URL, key: string) => {
   }
 
   return param.split(",").filter((p) => isValidColorCode(p)).map((p) => `#${p}`);
+};
+
+const snakeToCamel = (s: string) => {
+  return s.replace(/(_\w)/g, function (m) {
+    return m[1].toUpperCase();
+  });
+};
+
+const getParams = (url: URL) => {
+  const bottomLineTextsParam = url.searchParams.get("bottom_line_texts");
+  const topLineTextsParam = url.searchParams.get("top_line_texts");
+  const bottomLineTexts = bottomLineTextsParam ? bottomLineTextsParam.split(",") : [];
+  const topLineTexts = topLineTextsParam ? topLineTextsParam.split(",") : ["swfz", "til"];
+
+  const colorParams = ["cursor_color", "prompt_color", "bg_color", "title_color", "command_color"].reduce(
+    (acc, key) => {
+      acc[snakeToCamel(key)] = getColor(url, key) ?? defaultTheme[snakeToCamel(key)];
+
+      return acc;
+    },
+    {},
+  );
+
+  const statusLineColorParams = [
+    "top_line_bg_color",
+    "top_line_text_color",
+    "bottom_line_bg_color",
+    "bottom_line_text_color",
+    "top_line_item_colors",
+    "bottom_line_item_colors",
+  ].reduce((acc, key) => {
+    const statusLine = key.includes("top_") ? "topStatusLine" : "bottomStatusLine";
+    if (key.endsWith("_colors")) {
+      acc[snakeToCamel(key)] = getColors(url, key) ?? defaultTheme[statusLine].itemColors;
+    } else {
+      const k = snakeToCamel(key.split("_").slice(2).join("_"));
+      acc[snakeToCamel(key)] = getColor(url, key) ?? defaultTheme[statusLine][k];
+    }
+    return acc;
+  }, {});
+
+  return {
+    topLineTexts,
+    bottomLineTexts,
+    ...colorParams,
+    ...statusLineColorParams,
+    title: url.searchParams.get("title") ?? "No Title",
+    bottomLineBgText: url.searchParams.get("bottom_line_bg_text") ?? "Tags",
+    topLineBgText: url.searchParams.get("top_line_bg_text") ?? null,
+  };
 };
 
 const handler = async (request: Request): Promise<Response> => {
@@ -47,32 +86,10 @@ const handler = async (request: Request): Promise<Response> => {
     });
   }
 
-  const title = url.searchParams.get("title") ?? "No Title";
-  const bottomLineTextsParam = url.searchParams.get("bottom_line_texts");
-  const bottomLineTexts = bottomLineTextsParam ? bottomLineTextsParam.split(",") : [];
-  const topLineTextsParam = url.searchParams.get("top_line_texts");
-  const topLineTexts = topLineTextsParam ? topLineTextsParam.split(",") : ["swfz", "til"];
-
-  const bottomLineBgText = url.searchParams.get("bottom_line_bg_text") ?? "Tags";
-  const topLineBgText = url.searchParams.get("top_line_bg_text") ?? null;
-
-  const topLineItemColors = colorsParamGetter(url, "top_line_item_colors") ?? defaultTheme.topStatusLine.itemColors;
-  const topLineBgColor = colorParamGetter(url, "top_line_bg_color") ?? defaultTheme.topStatusLine.bgColor;
-  const topLineTextColor = colorParamGetter(url, "top_line_text_color") ?? defaultTheme.topStatusLine.textColor;
-  const bottomLineItemColors = colorsParamGetter(url, "bottom_line_item_colors") ??
-    defaultTheme.bottomStatusLine.itemColors;
-  const bottomLineBgColor = colorParamGetter(url, "bottom_line_bg_color") ?? defaultTheme.bottomStatusLine.bgColor;
-  const bottomLineTextColor = colorParamGetter(url, "bottom_line_text_color") ??
-    defaultTheme.bottomStatusLine.textColor;
-  const cursorColor = colorParamGetter(url, "cursor_color") ?? defaultTheme.cursorColor;
-  const promptColor = colorParamGetter(url, "prompt_color") ?? defaultTheme.promptColor;
-  const bgColor = colorParamGetter(url, "bg_color") ?? defaultTheme.bgColor;
-  const titleColor = colorParamGetter(url, "title_color") ?? defaultTheme.titleColor;
-  const commandColor = colorParamGetter(url, "command_color") ?? defaultTheme.commandColor;
-
-  console.log("title:", title);
-
   const [width, height] = [1200, 630];
+  const params = getParams(url);
+
+  console.log("title:", params.title);
 
   const canvas = createCanvas(width, height);
   const ctx = canvas.getContext("2d");
@@ -83,22 +100,7 @@ const handler = async (request: Request): Promise<Response> => {
   });
   ctx.font = "50pt notosans";
 
-  renderBackground(ctx, bgColor, canvas.width, canvas.height);
-  renderTopStatusLine(ctx, topLineTexts, topLineItemColors, topLineBgColor, topLineBgText, topLineTextColor);
-  renderPrompt(ctx, 10, textLineBase * 2, promptColor);
-  renderCommand(ctx, 80, textLineBase * 2, commandColor);
-  const titleLines = breakLines(ctx, title, 1150);
-  renderTitle(ctx, titleLines, 50, 300, titleColor);
-  renderCursor(ctx, titleLines, 50, 100, cursorColor);
-  renderBottomStatusLine(
-    ctx,
-    bottomLineTexts,
-    titleLines,
-    bottomLineItemColors,
-    bottomLineBgColor,
-    bottomLineBgText,
-    bottomLineTextColor,
-  );
+  render(ctx, { ...params, width, height });
 
   const headers = new Headers();
   headers.set("content-type", "image/png");
